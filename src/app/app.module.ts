f607887923a25c2780a5fac1c62e31583930a37e
AlouiOmar97 <60584294+AlouiOmar97@users.noreply.github.com>
2022-11-29 00:55:21 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AnnonceAppRoutingModule } from './annonce/annonce-app-routing.module';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProduitComponent } from './produit/produit.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { DetailComponent } from './detail/detail.component';
+import { AnnonceModule } from './annonce/annonce.module';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProduitComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    DetailComponent,
+    AddProduitComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AnnonceModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './../model/Product';
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.component.html',
+  styleUrls: ['./produit.component.css']
+})
+export class ProduitComponent implements OnInit {
+
+  listProdcut: Product[]=[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.listProdcut=[
+      {id: 1, title: "T-shirt 1", price: 18, quantity: 0, like: 0},
+      {id: 2, title: "T-shirt 2", price: 21, quantity: 10, like: 0},
+      {id: 3, title: "T-shirt 3", price: 16, quantity: 8, like: 0}, ]
+  }
+
+}
